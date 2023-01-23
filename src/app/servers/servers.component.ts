ba3264a9eb8aed58f574d6526160e6f7bfd0ac9e
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-01-23 09:30:59 +0100

    updates

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,33 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  userName = '';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server has been created. Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+    // this.serverName = event.target.value;
+  }
+
+  disableButton() {
+    if (this.userName === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  resetUsername() {
+    this.userName = '';
   }
 }
